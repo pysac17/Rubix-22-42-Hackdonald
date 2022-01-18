475e2e9c9c7568d1aff2ceceefff7074758566d7
arjunsathaye <arjun.sathaye@xaviers.edu.in>
2022-01-18 23:56:22 +0530

    update

--- process.py
+++ process.py
@@ -4,14 +4,14 @@ opens=open(r"C:\Users\arjun\Desktop\rizfood (1).json")
 reads=opens.read()
 loads=json.loads(reads)
 
-ingredient = ['tomato', 'cheese', 'milk','bread']
+ingredient = ['tomato', 'cheese', 'milk','bread'] 
 length = len(ingredient)
-confirm = [x*0 for x in range(length)]
-indexes = [str(x+1) for x in range(596)]
+confirm = [x*0 for x in range(length)] # 0, 1, 0, 1
+indexes = [str(x+1) for x in range(150)]
 totals = [x*0 for x in range(len(indexes))]
 fin = ""
 
-for i in range(596):
+for i in range(150):
     for k in range(length):
         for j in range(len(loads[indexes[i]]['ingredients'])):
             if ingredient[k] in loads[indexes[i]]['ingredients'][j]:
@@ -23,13 +23,14 @@ for i in range(596):
     for b in range(length): confirm[b] = 0
     
 max = 0
-for l in range(596):
+for l in range(150):
     if totals[l]>max: max = totals[l]
 
 for m in range(max):
-    for n in range(596):
+    for n in range(150):
         if totals[n] == max:
             fin = fin + str(n + 1) + '.'
     max = max - 1
 
-print(fin)
+finali = fin.split('.')
+print(finali)
