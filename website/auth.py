eb84d2778dd886e955777e8e580c5e4dc6e0ffd3
Sachi017 <80409697+pysac17@users.noreply.github.com>
2022-01-19 18:25:02 +0530

    Co-authored-by: Riz Lala <Rizlala123@users.noreply.github.com>

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,15 @@
 from flask import Blueprint,render_template, request, flash
 import mysql.connector
+import re
+from sqlalchemy import true   
+
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  
+
+def check(email):   
+    if(re.search(regex,email)):   
+        return True  
+    else:   
+        return False 
 
 auth = Blueprint('auth', __name__)
 
@@ -26,7 +36,7 @@ def signUp():
         password = request.form.get('password')
         password1 = request.form.get('password1')
         
-        if len(email) < 4:
+        if not check(email):
             flash('Enter a valid email!', category = 'error')
         elif len(firstName) < 3:
             flash('First name must be greater than 2 characters',category='error')
@@ -35,7 +45,7 @@ def signUp():
         elif len(password) < 8:
             flash('Password must be atleast 8 characters',category='error')
         else:
-            mydb = mysql.connector.connect(host="localhost", user="root",passwd="cybereagle0602",database="hax")
+            mydb = mysql.connector.connect(host="localhost", user="root",passwd="1234",database="hax")
             mycursor=mydb.cursor()
             mycursor.execute("INSERT INTO temp_users (FIRSTNAME,LASTNAME,EMAIL,PASSWORD) VALUES (%s,%s,%s,%s)",(firstName,lastName,email,password))
             mydb.commit()
@@ -44,7 +54,6 @@ def signUp():
             for i in mycursor:
                 print(i)
 
-            
             flash('Account created!',category='success')
 
             
